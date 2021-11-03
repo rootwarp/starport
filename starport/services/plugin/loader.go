acd0b157ed0e7b60ce8377b5a161a0460c0f8152
Joonkyo Kim <rootwarp@gmail.com>
2021-11-03 12:59:53 +0000

    plugin start

--- starport/services/plugin/loader.go
+++ starport/services/plugin/loader.go
@@ -1,1 +1,19 @@
 package plugin
+
+// Loader provides plugin control interfaces.
+type Loader interface {
+	Load(pluginName string) error
+}
+
+type loader struct {
+}
+
+func (l *loader) Load(name string) error {
+	return nil
+}
+
+// NewLoader creates plugin loader.
+func NewLoader() Loader {
+	// TODO: Need default or config parameters to get path configs.
+	return nil
+}
